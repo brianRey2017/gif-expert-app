54ce1224a406e037e7d07caf82e6f6fa4922b604
brianRey2017 <brianRey2017@users.noreply.github.com>
2021-07-07 19:28:15 +0000

    Prettified Code!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { GifExpertApp } from "App";
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { GifExpertApp } from 'App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-ReactDOM.render(<GifExpertApp />, document.getElementById("root"));
+ReactDOM.render(<GifExpertApp />, document.getElementById('root'));
